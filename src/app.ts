f9b49eb4ae54a5c68c5cb8a488adc0434586e438
faceless49 <digital.zeus@mail.ru>
2022-03-01 22:39:29 +0300

    make socket channels for clients some typed, name sent, message sent

--- src/app.ts
+++ src/app.ts
@@ -10,16 +10,51 @@ const socket = new Server(server, {
   }
 });
 
+
+const messages: Array<any> = []
+const usersState = new Map() // We save all usersState in this collection
 app.get('/', (req, res) => {
   res.send('Hello, its WS lite server');
 });
 
 
 socket.on('connection', (socketChannel) => { // if client connect =>
-  socketChannel.on('client-message-sent', (message: string) => {
-    console.log(message)
+  usersState.set(socketChannel, {id: new Date().getDate().toString(), name: 'anonym'})
+
+  socket.on('disconnect', () => {
+    usersState.delete(socketChannel)      // when user disconnect we delete our channel with him
   })
 
+  socketChannel.on('client-name-sent', (name: string) => { // set name user
+    const user = usersState.get(socketChannel)                            // We needed to save users for socketChannel
+    user.name = name                                                 // When socketChannel create(user coming) we get user and change name from closure
+  })
+
+
+  socketChannel.on('client-message-sent', (message: string, successFn) => {
+    if (typeof message !== 'string' || message.length > 20) {
+      successFn('Message length should be less than 20 chars')
+      return
+    }
+
+
+    const user = usersState.get(socketChannel)
+    let messageItem = {
+      message: message, id: new Date().getDate().toString(),
+      user: {id: user.id, name: user.name}
+    }
+    messages.push(messageItem)
+    socket.emit('new-message-sent', messageItem)
+
+    successFn(null)
+  })
+  socketChannel.emit('init-messages-published', messages, () => {
+    console.log('INIT MESSAGE RECEIVED')
+  })
+
+  socketChannel.on('client-typed', () => {
+    socketChannel.broadcast.emit('user-typed-message', usersState.get(socketChannel))
+  })
 });
 
 const PORT = process.env.PORT || 3009
